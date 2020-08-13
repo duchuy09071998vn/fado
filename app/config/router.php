2d26dfb4a50f5450df81b03dd179a0dc01042113
Nguyễn Đức Huy <duchuy9798@gmail.com>
2020-08-13 15:21:29 +0700

    add route

--- app/config/router.php
+++ app/config/router.php
@@ -11,5 +11,7 @@ $router->add('/user/login', ['controller' => 'user', 'action' => 'login']);
 $router->add('/user/login/submit', ['controller' => 'user', 'action' => 'loginSubmit']);
 $router->add('/signup/register', ['controller' => 'user', 'action' => 'register']);
 $router->add('/signup/register/submit', ['controller' => 'user', 'action' => 'registerSubmit']);
+$router->add('/user/profile', ['controller' => 'user', 'action' => 'profile']);
+$router->add('/user/logout', ['controller' => 'user', 'action' => 'logout']);
 
 $router->handle();
